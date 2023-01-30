53dd78e18776f9272ea84d5f83854aa4d3776ca9
ElmiraZairbekova <loveni-shop@yandex.ru>
2023-01-31 00:59:18 +0300

    fix

--- models/movie.js
+++ models/movie.js
@@ -34,7 +34,7 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-      validator: (v) => validator.isURL(v),
+      validator: (url) => isUrl(url),
       message: 'Некорректный URL',
     },
   },
