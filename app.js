ec2b72b33f281212a72e4573a3de9688af6faf9f
ElmiraZairbekova <loveni-shop@yandex.ru>
2023-01-31 00:00:25 +0300

    fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors);
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
